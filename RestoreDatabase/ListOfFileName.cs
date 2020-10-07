8d5bea0d785af7135016a67ef5afe67e3ad8653f
Fred <domaine75@gmail.com>
2020-10-07 20:05:20 +0200

    Add Code to delete duplicate full files

--- RestoreDatabase/ListOfFileName.cs
+++ RestoreDatabase/ListOfFileName.cs
@@ -7,6 +7,7 @@ namespace RestoreDatabase
     public List<FileName> ListOfFiles { get; set; }
     public List<FileName> ListOfFull { get; set; }
     public List<FileName> ListOfDiff { get; set; }
+    public List<FileName> ListOfFilesToBeDeleted { get; set; }
     public bool HasSeveralFull { get; set; }
 
     public ListOfFileName()
@@ -14,6 +15,7 @@ namespace RestoreDatabase
       ListOfFiles = new List<FileName>();
       ListOfFull = new List<FileName>();
       ListOfDiff = new List<FileName>();
+      ListOfFilesToBeDeleted = new List<FileName>();
       HasSeveralFull = false;
     }
   }
