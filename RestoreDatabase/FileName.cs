20b0d03ca880dae645fd64509861280935d8a473
fred <domaine75@gmail.com>
2020-10-08 09:39:26 +0200

    Update FileName.cs

--- RestoreDatabase/FileName.cs
+++ RestoreDatabase/FileName.cs
@@ -65,7 +65,7 @@ namespace RestoreDatabase
 
     private DateTime CalculateDate(string name)
     {
-      // GESTAMI_X_2_backup_2020_10_02_20_51_40_320.full
+      // Gestion_X_2_backup_2020_10_02_20_51_40_320.full
       var longDate = name.Split('_');
       DateTime result = new DateTime(int.Parse(longDate[4]), int.Parse(longDate[5]), int.Parse(longDate[6]));
       return result;
