0f82ab80a18da9b8b41cd5ccaeb953b5cade534e
Fred <domaine75@gmail.com>
2020-10-12 10:01:29 +0200

    Add Is-File-Locked method

--- RestoreDatabase/Program.cs
+++ RestoreDatabase/Program.cs
@@ -218,6 +218,28 @@ GO
       Console.ReadKey();
     }
 
+    private bool IsFileLocked(FileInfo file)
+    {
+      try
+      {
+        using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+        {
+          stream.Close();
+        }
+      }
+      catch (IOException)
+      {
+        //the file is unavailable because it is:
+        //still being written to
+        //or being processed by another thread
+        //or does not exist (has already been processed)
+        return true;
+      }
+
+      //file is not locked
+      return false;
+    }
+
     private static List<string> GetOldestFileNames(List<string> value)
     {
       List<string> result = new List<string>();
